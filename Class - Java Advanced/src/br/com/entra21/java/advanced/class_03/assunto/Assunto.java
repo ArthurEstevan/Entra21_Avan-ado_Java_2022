22bf4ca009aa15618e1a5b79ab4caa3f32d48e66
Arthur Estevan Vargas <104040305+ArthurEstevan@users.noreply.github.com>
2022-06-09 18:28:46 -0300

    Commit

--- Class - Java Advanced/src/br/com/entra21/java/advanced/class_03/assunto/Assunto.java
+++
@@ -1,8 +0,0 @@
-package br.com.entra21.java.advanced.class_03.assunto;
-
-/**
-*aula 3 legal
-**/
-public @interface Assunto {
-
-}
--- Class - Java Advanced/src/br/com/entra21/java/advanced/class_03/assunto/Lembre.java
+++
@@ -1,7 +0,0 @@
-package br.com.entra21.java.advanced.class_03.assunto;
-/**
-*
-**/
-public @interface Lembre {
-	String value();
-}
